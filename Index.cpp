c33e9bd6aba06c592663fd53e1517ad66f01055f
Mauriciodeoliveira007 <mauriciodeoliveira06@gmail.com>
2025-07-25 15:58:54 -0300

    Recebendo primeiro chute

--- Index.cpp
+++ Index.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Jogo da adivinhação" <<endl;
+    //Criação de variáveis
+    int numeroSecreto = 0;
+    int chute = 0;
+    
+    cout << "Jogo da adivinhação" <<endl <<endl;
+
+    cout << "Escolha um número para chutar: ";
+    cin >> chute;
 }
